41d57e52d276c176408c4c4c600593dd78b5b252
Parag Gattani <53811272+paraggattani28@users.noreply.github.com>
2020-12-11 10:07:08 +0530

    Update LAB8.py

--- LAB8/LAB8.py
+++ LAB8/LAB8.py
@@ -1,82 +1,47 @@
-#djisktras algorithm for shortest pathfrom source to destination
-from collections import defaultdict
+#include <stdio.h>
+#define gen 0x11021
+long int msb(long int temp);
+long int checksum(long int frame);
+int main()
+{
+    long int opframe, inframe, flag, r_frame;
+    printf("Enter the frame to be transmitted in hex:");
+    scanf("%lx", &inframe);
+    inframe = inframe << 16;
+    opframe = inframe ^ (checksum(inframe));
+    printf("The frame to be transmitted is %lx\n", opframe);
+    printf("Enter the received frame:");
+    scanf("%lx", &r_frame);
+    printf("For the received frame\n ");
+    flag = checksum(r_frame);
+    if (flag > 0)
+        printf("\nError!!!!\n");
+    else
+        printf("\ndata received is error free\n");
+}
+long int checksum(long int frame)
+{
+    long int posit_frame, posit_gen, g = gen, g1, temp;
+    posit_frame = msb(frame);
+    posit_gen = msb(g);
+    while (posit_gen <= posit_frame)
+    {
 
-class Graph():
-    def __init__(self):
-        """
-        self.edges is a dict of all possible next nodes
-        e.g. {'X': ['A', 'B', 'C', 'E'], ...}
-        self.weights has all the weights between two nodes,
-        with the two nodes as a tuple as the key
-        e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
-        """
-        self.edges = defaultdict(list)
-        self.weights = {}
-    
-    def addEdge(self, from_node, to_node, weight):
-        # Note: assumes edges are bi-directional
-        self.edges[from_node].append(to_node)
-        self.edges[to_node].append(from_node)
-        self.weights[(from_node, to_node)] = weight
-        self.weights[(to_node, from_node)] = weight
-
-def dijsktra(graph, initial, end):
-    # shortest paths is a dict of nodes
-    # whose value is a tuple of (previous node, weight)
-    shortest_paths = {initial: (None, 0)}
-    current_node = initial
-    visited = set()
-    
-    while current_node != end:
-        visited.add(current_node)
-        destinations = graph.edges[current_node]
-        weight_to_current_node = shortest_paths[current_node][1]
-
-        for next_node in destinations:
-            weight = graph.weights[(current_node, next_node)] + weight_to_current_node
-            if next_node not in shortest_paths:
-                shortest_paths[next_node] = (current_node, weight)
-            else:
-                current_shortest_weight = shortest_paths[next_node][1]
-                if current_shortest_weight > weight:
-                    shortest_paths[next_node] = (current_node, weight)
-        
-        next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
-        if not next_destinations:
-            return "Route Not Possible"
-        # next node is the destination with the lowest weight
-        current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
-    
-    # Work back through destinations in shortest path
-    path = []
-   
-    while current_node is not None:
-        path.append(current_node)
-       
-        next_node = shortest_paths[current_node][0]
-        current_node = next_node
-        
-        
-    # Reverse path
-    path = path[::-1]
-    print('Shortest Weigth:', current_shortest_weight)
-    print (path)
-
-
-# Test using Above Picture
-
-g = Graph()
-
-# Add edges with weight
-g.addEdge('a', 'b', 4)
-g.addEdge('a', 'c', 2)
-g.addEdge('b', 'c', 1)
-g.addEdge('b', 'd', 5)
-g.addEdge('c', 'd', 8)
-g.addEdge('c', 'e', 10)
-g.addEdge('d', 'e', 2)
-g.addEdge('d', 'z', 6)
-g.addEdge('e', 'z', 5)
-
-# Dijkstras Algo
-dijsktra(g, 'a', 'z')
+        g1 = g << (posit_frame - posit_gen);
+        frame = g1 ^ frame;
+        posit_frame = msb(frame);
+    }
+    printf("The checksum is %lx\n", frame);
+    return (frame);
+}
+long int msb(long int temp)
+{
+    int i = 0;
+    if (temp == 0)
+        return 0;
+    while (temp > 0)
+    {
+        temp = temp << 1;
+        i++;
+    }
+}
